c16d00e9599c2d0963eaeddf3308281045f356d1
Sandra Tirado <Sondi@Sandras-MacBook-Pro.local>
2015-07-18 13:13:59 -0500

    Welcome view displays all articles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   	def logged_in?
     	!current_user.nil?
   	end
-
+    
     helper_method :current_user
     helper_method :logged_in?
 end
