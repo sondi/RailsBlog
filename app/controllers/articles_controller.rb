d81ba197b50e84e583f7e1fada105fac07e341cd
Sandra Tirado <Sondi@Sandras-MacBook-Pro.local>
2015-07-17 23:16:40 -0500

    lay out links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,13 @@
 class ArticlesController < ApplicationController
 	
 	def index
-    	@articles = current_user.articles
+    	@articles = Article.all
 
   	end
 
 	def show
     	@article = Article.find(params[:id])
   	end
-
 
 	def edit
     	@article = Article.find(params[:id])
